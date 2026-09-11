e2bcbc263254c50da9160bf82c6c67185acec954
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-09-11 16:39:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -13,7 +13,6 @@ class Solution {
                     dp[i][j] = 1+dp[i-1][j-1];
                     ans = max(ans,dp[i][j]);
                 } 
-                else dp[i][j] = 0;
             }
         }
         
